13ca9b326c2cbdb67689d79c38593ceb4925d516
Alexander Stevenson <alexanderfstevenson@aol.com>
2019-10-21 17:47:25 +0100

    adds the function min()

--- builtInFunctions2.py
+++ builtInFunctions2.py
@@ -1,5 +1,10 @@
 from time import sleep
 grades = [15,33,37,66,89,92,94,97]
+#printing the highest grade
 print("the highest grade % was:")
-sleep(1)
+sleep(0.5)
 print(max(grades))
+#Printing the minimum grade:
+print("And the lowest grade was:")
+sleep(0.5)
+print(min(grades))
