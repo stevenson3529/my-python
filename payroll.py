034d8ca03bd658f5bdaff635f1c4706a93327793
Alexander Stevenson <alexanderfstevenson@aol.com>
2019-10-21 17:59:51 +0100

    Calculatees the total payroll of a company

---
+++ payroll.py
@@ -0,0 +1,19 @@
+'''
+Program that calculates the total payroll in a company
+'''
+done = False
+#a list of all the salaries
+salaries = [10,50,250,400,30,999555000,189,5050,2000,25,3845,9898]
+
+#Lets a user add a salary to the list
+while done != True:
+    add = input("Enter a salary to add to the list, or type no to cancel.  >")
+    if add.isnumeric() == True:
+        add = int(add)
+        salaries.append(add)
+    elif add.upper() == 'NO':
+        done = True
+    else:
+        print("Enter a number, or type no to cancel.")
+
+print("The total payroll is £", sum(salaries))
