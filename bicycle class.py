e7ceb80c36e1e1a20da5a4d90cf10d36e3f83425
Alexander Stevenson <alexanderfstevenson@aol.com>
2019-11-18 17:41:54 +0000

    Creates a new class for bicycles.

---
+++ bicycle class.py
@@ -0,0 +1,13 @@
+'''
+
+'''
+class Bicycle():
+    def hasWheels(self):
+        print("I have two wheels.")
+    def hasSeat(self):
+        print("I have a seat.")
+
+racingBike = Bicycle()
+
+racingBike.hasWheels()
+    
