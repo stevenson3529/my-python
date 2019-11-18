969d2460b0acc440342f5d6b5f41178871ac7d80
Alexander Stevenson <alexanderfstevenson@aol.com>
2019-11-18 17:29:59 +0000

    adds a class with two methods

---
+++ class.py
@@ -0,0 +1,12 @@
+'''
+Introduction to classes and objects
+'''
+
+class Superhero():
+    def fly(self):
+        print("i can fly")
+        
+    def hotDog(self,num):
+        print("I can eat", num , "hotdogs in a minute.")
+    
+    
