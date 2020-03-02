1feef86806c86669ccc9529627ec4dd8d0c3fbbd
Alexander Stevenson <alexanderfstevenson@aol.com>
2020-03-02 17:54:21 +0000

    A program that prints the digits of pi

---
+++ pi_digits.py
@@ -0,0 +1,28 @@
+'''
+A program that prints the digits of pi
+'''
+
+filename = 'assets\pi_digits.txt'
+#opens the file
+with open('assets\pi_digits.txt') as digits_file:
+    '''
+    digits = digits_file.read()
+    print(digits)
+    for line in digits_file:
+        print(line.rstrip())
+    '''
+    lines = digits_file.readlines()
+
+#loops through all the lines and prints them
+'''
+for line in lines:
+    print(line.rstrip())
+'''
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string[:56])
+print('----------------')
+print(len(pi_string))
