ca2da3aff757bfc49355739ca13c6a7a54f91ec4
Alexander Stevenson <alexanderfstevenson@aol.com>
2020-03-31 15:56:33 +0100

    A module for user input with validation.

---
+++ user_input.py
@@ -0,0 +1,33 @@
+'''
+A module for user inputs, with validation.
+'''
+
+#q = question
+#t = type (s = string, n = number, b = boolean)
+def ask(q,t):
+    if t == "s":
+        print(q)
+        userInput = input(">")
+        return userInput
+    elif t == "n":
+        while True:
+            print(q)
+            userInput = input(">")
+            if userInput.isnumeric() == True:
+                return int(userInput)
+            else:
+                print("Not a valid input")
+    elif t == "b":
+        while True:
+            print(q)
+            userInput = input(">")
+            if userInput in yesses:
+                return True
+            elif userInput in nos:
+                return False
+            else:
+                print("Not a valid input")
+    else:
+        print("Sorry, an error has occured in the 'user_input' module.")
+
+
