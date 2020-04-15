63dca9261217081a72ce6c5d530cd297a331b2dd
Alexander Stevenson <alexanderfstevenson@aol.com>
2020-04-15 16:20:29 +0100

    Fixes spelling erorors

--- pi_digits_birthday.py
+++ pi_digits_birthday.py
@@ -18,4 +18,4 @@ usersBDay = user_input.ask("Please enter your birthday in the form ddmmyy","s")
 if usersBDay in pi_string:
     print("Your birthday appears in the first million digits of pi!")
 else:
-    print("Sorry, your birthday does not apepar to be in the first million digts of pi.")
+    print("Sorry, your birthday does not appear in the first million digts of pi.")
